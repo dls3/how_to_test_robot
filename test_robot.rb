802d154421e41c987a342543a04b5269f179c5ac
Doug <doug.steinberg@gmail.com>
2017-06-22 00:12:41 -0400

    First test complete

--- test_robot.rb
+++ test_robot.rb
@@ -5,18 +5,23 @@ require './robot.rb'
 class TestRobot < MiniTest::Test
 
   def test_that_foreign_vintage_robot_needing_repairs_sent_to_station_1
-    skip
     # arrange
-    
+    robot = Robot.new
+    robot.foreign_model = true
+    robot.vintage_model = true
+    robot.needs_repairs = true
+
     # act
+    var = robot.station
 
     # assert
+    assert_equal(1 , var)
   end
 
   def test_that_foreign_robot_neeing_repairs_sent_to_station_1
     skip
     # arrange
-    
+
     # act
 
     # assert
@@ -25,7 +30,7 @@ class TestRobot < MiniTest::Test
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
     skip
     # arrange
-    
+
     # act
 
     # assert
@@ -34,25 +39,25 @@ class TestRobot < MiniTest::Test
   def test_that_standard_robot_needing_repairs_sent_to_station_3
     skip
     # arrange
-    
+
     # act
 
     # assert
   end
-  
+
   def test_that_robot_in_good_condition_sent_to_station_4
     skip
     # arrange
-    
+
     # act
 
     # assert
   end
-  
+
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
     skip
     # arrange
-    
+
     # act
 
     # assert
@@ -61,7 +66,7 @@ class TestRobot < MiniTest::Test
   def test_prioritize_tasks_with_todos_returns_max_todo_value
     skip
     # arrange
-    
+
     # act
 
     # assert
@@ -70,7 +75,7 @@ class TestRobot < MiniTest::Test
   def test_workday_on_day_off_returns_true
     skip
     # arrange
-    
+
     # act
 
     # assert
@@ -79,7 +84,7 @@ class TestRobot < MiniTest::Test
   def test_workday_not_day_off_returns_false
     skip
     # arrange
-    
+
     # act
 
     # assert
