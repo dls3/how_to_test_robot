8725da80c17bdf7bebb64d8ce301a9204914672b
Doug <doug.steinberg@gmail.com>
2017-06-22 00:20:37 -0400

    first 5 tests complete

--- test_robot.rb
+++ test_robot.rb
@@ -10,48 +10,51 @@ class TestRobot < MiniTest::Test
     robot.foreign_model = true
     robot.vintage_model = true
     robot.needs_repairs = true
-
     # act
     var = robot.station
-
     # assert
     assert_equal(1 , var)
   end
 
   def test_that_foreign_robot_neeing_repairs_sent_to_station_1
-    skip
     # arrange
-
+    robot = Robot.new
+    robot.foreign_model = true
+    robot.needs_repairs = true
     # act
-
+    var = robot.station
     # assert
+    assert_equal(1, var)
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
     # arrange
-
+    robot = Robot.new
+    robot.vintage_model = true
+    robot.needs_repairs = true
     # act
-
+    var = robot.station
     # assert
+    assert_equal(2, var)
   end
 
   def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
     # arrange
-
+    robot = Robot.new
+    robot.needs_repairs = true
     # act
-
+    var = robot.station
     # assert
+    assert_equal(3, var)
   end
 
   def test_that_robot_in_good_condition_sent_to_station_4
-    skip
     # arrange
-
+    robot = Robot.new
     # act
-
+    var = robot.station
     # assert
+    assert_equal(4, var)
   end
 
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
